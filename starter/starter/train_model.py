b1bc244ba43fa46afb3d761195c0c1131abbe71c
Steph11 <stephanetekamfeudjo@Stephanes-MBP.fritz.box>
2021-11-12 23:27:59 +0100

    train and save model

--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -1,12 +1,14 @@
 # Script to train machine learning model.
-
+import joblib
 from sklearn.model_selection import train_test_split
+import pandas as pd
+from .ml.data import process_data
+from.ml.model import train_model
 
-# Add the necessary imports for the starter code.
+# Load data
+data = pd.read_csv("census_copy.csv")
 
-# Add code to load in the data.
-
-# Optional enhancement, use K-fold cross validation instead of a train-test split.
+# Train-test split.
 train, test = train_test_split(data, test_size=0.20)
 
 cat_features = [
@@ -23,6 +25,17 @@ X_train, y_train, encoder, lb = process_data(
     train, categorical_features=cat_features, label="salary", training=True
 )
 
-# Proces the test data with the process_data function.
+# Proces the test data.
+X_test, y_test, encoder, lb = process_data(
+    test,
+    categorical_features=cat_features,
+    label="salary",
+    training=False,
+    encoder=encoder,
+    lb=lb
+)
 
-# Train and save a model.
+# Train and save the model.
+clf= train_model(X_train, y_train)
+joblib.dump(clf, 'clf_model.pkl')
+
